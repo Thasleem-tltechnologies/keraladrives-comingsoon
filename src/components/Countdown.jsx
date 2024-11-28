fdcf8895d28e633a80f5018b20b399b00e983f6a
Thasleem-tlechnologies <thasleem.tltechnologies@gmail.com>
2024-11-28 16:13:58 +0530

    Correct CSS import filename in Countdown component

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import '../styles/Countdown.css';
+import '../styles/CountDown.css';
 
 const Countdown = () => {
   const [timeLeft, setTimeLeft] = useState({
